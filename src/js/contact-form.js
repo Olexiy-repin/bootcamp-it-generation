c2f5cd8d1fc60dec64ab7262ebf021259f97f58b
Olexiy-repin <repinlyoha@gmail.com>
2022-11-10 12:01:14 +0200

    classwork

--- src/js/contact-form.js
+++ src/js/contact-form.js
@@ -1,13 +1,58 @@
+import localstorageApi from './localstorage';
+
 const contactFormEl = document.querySelector('.js-contact-form');
 const userData = {};
 
-const user = {
-  firstName: 'Carolyn',
-  lastName: 'Griffin',
-  age: 50,
-  email: 'piggaz@gazmiz.za',
+const fillContactFormElements = () => {
+  const userDataFromLS = localstorageApi.load('userData');
+
+  for (const prop in userDataFromLS) {
+    if (userDataFromLS.hasOwnProperty(prop)) {
+      console.log(prop);
+
+      contactFormEl.elements[prop].value = userDataFromLS[prop];
+    }
+  }
 };
 
-localStorage.setItem('userData', JSON.stringify(user));
+fillContactFormElements();
 
-console.log(localStorage);
+const onContactFormElChange = event => {
+  const name = event.target.name;
+  const value = event.target.value;
+
+  userData[name] = value;
+
+  localstorageApi.save('userData', userData);
+};
+
+const onContactFormSubmit = event => {
+  event.preventDefault();
+
+  event.target.reset();
+  localstorageApi.remove('userData');
+};
+
+contactFormEl.addEventListener('change', onContactFormElChange);
+contactFormEl.addEventListener('submit', onContactFormSubmit);
+
+// const user = {
+//   firstName: 'Carolyn',
+//   lastName: 'Griffin',
+//   age: 50,
+//   email: 'piggaz@gazmiz.za',
+// };
+
+// localStorage.setItem('userData', JSON.stringify(user));
+// localStorage.setItem(
+//   'users',
+//   JSON.stringify([{ firstName: 'Oleksii' }, { firstName: 'Oleksii' }, { firstName: 'Oleksii' }])
+// );
+
+// console.log(window.localStorage);
+
+// console.log(JSON.parse(localStorage.getItem('userData')));
+
+// localStorage.removeItem('userData');
+
+// localStorage.clear();
